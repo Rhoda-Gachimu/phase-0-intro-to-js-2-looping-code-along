2a0fe5c4f2617a0203fb0a76cb7dd25d1e6e1e2c
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-08-15 13:52:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let messages = []
+
+function writeCards(array, msg) {
+    for (let i=0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${msg} gift!`)
+    }
+    return messages
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num)
+        num--
+    }
+}
